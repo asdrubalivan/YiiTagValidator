aef77a0a0e21020067137172af5ff8a909d32ec9
Asdrúbal Iván Suárez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2014-01-23 23:07:50 -0430

    TagValidator file uploaded

---
+++ TagValidator.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * A small validator for attributes used with ETaggable
+ * @author asdrubalivan
+ */
+
+class TagValidator extends CValidator{
+    public $max=null;
+    public $min=null;
+    protected function validateAttribute($object, $attribute) {
+        $arrayTags = explode(',', $object->$attribute);
+        foreach($arrayTags as $tag)
+        {
+            $lenTag = strlen($tag);
+            if($this->max !== null && $lenTag > $this->max)
+            {
+                $this->addError($object, $attribute, 
+                        "Tag $tag is too long. Maximum is $this->max");
+            }
+            elseif($this->min !== null && $lenTag < $this->min)
+            {
+                $this->addError($object, $attribute, 
+                        "Tag $tag is too short. Minimum is $this->max");
+            }
+        }
+    }    
+}
